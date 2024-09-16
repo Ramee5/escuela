f244308b77161c193518583c9c48b7154beeaa54
bauti <47864038@insi.edu.ar>
2024-09-16 14:54:26 -0300

    Escuela v1.0

---
+++ alta materia.php
@@ -0,0 +1,20 @@
+<?php
+//conectar a la bd
+$conexion  = new mysqli("localhost","root","","diciembre");
+
+//recibimos datos de la formulario html
+$materia = $_POST("materia");
+
+//generamos la consulta para dar de alta en la bd
+$alta = "INSERT INTO materia(nombre) VALUE (`$materia`)"
+
+
+//ejecutamos la consulta 
+$conexion->query($alta);
+//verificamos el alta
+if($conexion){
+    echo "se ha dado de alta la materia".$materia;
+}else{
+    echo "no se ha dado de alta la materia".$materia;
+    
+}
